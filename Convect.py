204851b40f3d7e6e742b6013d3826cf7b4605b41
Dmitry <dmitri.klochkov.05@mail.ru>
2022-09-11 21:47:19 +0300

    csv to json

---
+++ Convect.py
@@ -0,0 +1,11 @@
+import csv
+import json
+
+csvfile = open('test.csv','r')
+jsonfile = open('test.json','w')
+
+reader = csv.DictReader(csvfile)
+for row in reader:
+    json.dump(row, jsonfile)
+
+
